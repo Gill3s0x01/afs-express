698de87cd1579f730b9da7b3255a8a1006e906cd
gilles30 <lorison.gilles@gmail.com>
2023-04-23 18:27:14 -0300

    feat: Config rules globals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/global'
+import { defaultTheme } from '../styles/themes/default'
+import { AppProps } from 'next/app'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default App
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,8 @@
+export const defaultTheme = {
+  primary: '#9F1427',
+  secondary: '#312F2F',
+  gray500: '#C7C7C7',
+  gray300: '#E1E1E1',
+  white: '#FFFFFF',
+  black: '#000000',
+}
